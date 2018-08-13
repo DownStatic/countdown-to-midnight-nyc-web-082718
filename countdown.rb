a634959413b50fd2b6edf6043bfcf0e25013a800
Christopher Stull <chris.stull@gmail.com>
2018-08-13 16:18:36 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(num)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countown_with_sleep(num)
+  while num>0 do
+    puts "#{num} SECOND(S)!"
+    wait 1
+    num-=1
+  end
+  return "HAPPY NEW YEAR!"
+end
